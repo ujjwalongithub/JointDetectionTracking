7c334e078e6f8c197115a97283cfd33f342b57bf
Ujjwal <ujjwal@uni-hildesheim.de>
2022-10-15 17:46:14 +0200

    Corrected documentation

--- utils/vis_mot17.py
+++ utils/vis_mot17.py
@@ -19,13 +19,12 @@ class UnNormalize(object):
     def __call__(self, tensor):
         """
         Args:
-            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+            tensor (Tensor): Tensor image of any size to be normalized
         Returns:
             Tensor: Normalized image.
         """
         for t, m, s in zip(tensor, self.mean, self.std):
             t.mul_(s).add_(m)
-            # The normalize code -> t.sub_(m).div_(s)
         return tensor
 
 
