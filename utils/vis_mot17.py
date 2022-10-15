3c729ceb34424bb2e313db14491cd23278562543
Ujjwal <ujjwal@uni-hildesheim.de>
2022-10-16 00:47:10 +0200

    Code to visualize data batches of MOT17

--- utils/vis_mot17.py
+++ utils/vis_mot17.py
@@ -1,9 +1,13 @@
 import argparse
 
+import cv2
+import imageio
+import numpy as np
+import torch
 from torch.utils.data import DataLoader
 
-import colors
 from datasets.mot17 import MOT17
+from utils import colors
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--root_path', required=True, help='Root folder where MOT17 dataset has been stored.')
@@ -28,12 +32,8 @@ class UnNormalize(object):
         return tensor
 
 
-def denormalize_image(image):
-    return UnNormalize()(image)
-
-
 def get_unique_tracks(gt):
-    unique_ids = set(gt[0, :, :, -1].flatten())
+    unique_ids = set(gt[0, :, :, -1].numpy().astype(int).flatten())
     unique_ids = unique_ids.difference([0])
     return list(unique_ids)
 
@@ -45,10 +45,35 @@ def prepare_color_dict(gt):
     return color_dict
 
 
-def denormalize_boxes(gt, padding):
-    boxes = gt[0, :, :(201 - padding), :]
-    boxes[:, :, :4] *= 512
-    return boxes
+def plot_boxes(images, gt, paddings, file_name):
+    imgs = list()
+    images = UnNormalize()(images)
+    color_dict = prepare_color_dict(gt)
+    print(color_dict)
+    for imgnum in range(images.shape[1]):
+        img = images[0, imgnum, :, :, :].numpy()
+        img = np.transpose(img, (1, 2, 0))
+        img = np.ascontiguousarray(img)
+        normalized_image = np.ascontiguousarray(np.zeros_like(img))
+        normalized_image = cv2.normalize(img, normalized_image, 0, 255, cv2.NORM_MINMAX)
+        normalized_image.astype(np.uint8)
+        frame_gt = gt[0, imgnum, :, :]
+        padding = paddings[0, imgnum]
+        boxes = frame_gt[:(200 - padding), :]
+        boxes[:, :4] *= 512
+        boxes = torch.round(boxes)
+        # boxes = np.round(boxes)
+        boxes = boxes.numpy()
+        boxes = boxes.astype(int)
+        for i in range(boxes.shape[0]):
+            clr = list(color_dict[boxes[i, 4]][::-1])
+            clr = [x * 255 for x in clr]
+            clr = list(map(int, clr))
+            img = cv2.rectangle(normalized_image, (boxes[i, 0], boxes[i, 1]), (boxes[i, 2], boxes[i, 3]), clr)
+
+        imgs.append(img)
+    imageio.mimwrite(file_name, imgs)
+    return None
 
 
 if __name__ == "__main__":
@@ -62,18 +87,9 @@ if __name__ == "__main__":
     loader = DataLoader(dataset=db, batch_size=1,
                         shuffle=True)
 
+    i = 1
     for data in loader:
         image, bbox, paddings = data
-        print(bbox.shape)
-        # print(image)
-        # print('-----')
-        # image2 = denormalize_image(image)
-        # image_to_write = image2[0,0,:,:,:]
-        # image_to_write = np.transpose(image_to_write,(1,2,0))
-        # print(image_to_write.shape)
-        # image_to_write = image_to_write.numpy()
-        # normalized_image = np.ascontiguousarray(np.zeros_like(image_to_write))
-        # print(normalized_image.shape)
-        # normalized_image = cv2.normalize(image_to_write, normalized_image,0, 255, cv2.NORM_MINMAX)
-        # cv2.imwrite('abc.jpg', normalized_image)
-        break
+        filename = 'batch_{}.gif'.format(i)
+        plot_boxes(image, bbox, paddings, filename)
+        i += 1
